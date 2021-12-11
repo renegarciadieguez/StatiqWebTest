251823ba6d03d0fe500ead3f8bfcc704bf43f56d
Rene Garcia <renegarciadieguez@users.noreply.github.com>
2021-12-10 19:48:07 -0800

    Updated pipeline to include deployment

--- StatiqWebTest/Program.cs
+++ StatiqWebTest/Program.cs
@@ -1,7 +1,14 @@
 ﻿using Statiq.App;
+using Statiq.Common;
 using Statiq.Web;
 
 await Bootstrapper
     .Factory
     .CreateWeb(args)
+    .DeployToGitHubPagesBranch(
+        "renegarciadieguez",
+        "renegarciadieguez.github.io/StatiqWebTest",
+        Config.FromSetting<string>("GITHUB_TOKEN"),
+        "gh-pages"
+    )
     .RunAsync();
