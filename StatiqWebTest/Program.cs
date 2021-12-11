03cf3fba488bf270bde07b435ae70ccd7eb4ad8d
Rene Garcia <renegarciadieguez@users.noreply.github.com>
2021-12-10 22:16:25 -0800

    Release (#4)
    * Build actions (#1)
    
    * Moved readme to the root
    * Added build action
    * Updated action for main branch
    
    * Pages (#3)
    
    * Updated pipeline to include deployment
    * Updated actions

--- StatiqWebTest/Program.cs
+++ StatiqWebTest/Program.cs
@@ -1,7 +1,14 @@
 ﻿using Statiq.App;
+using Statiq.Common;
 using Statiq.Web;
 
 await Bootstrapper
     .Factory
     .CreateWeb(args)
+    .DeployToGitHubPagesBranch(
+        "renegarciadieguez",
+        "StatiqWebTest",
+        Config.FromSetting<string>("GITHUB_TOKEN"),
+        "gh-pages"
+    )
     .RunAsync();
